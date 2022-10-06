b7d30a247949d6fe1a0c50523e161ba4519c1ecd
zac <zachary.gagnou@epfedu.fr>
2022-10-06 21:53:42 +0200

    added single dht to test

--- all_sensor_test.ino
+++ all_sensor_test.ino
@@ -1,6 +1,11 @@
 #include "max6675.h"
 #include "DHT.h"
 
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
 int thermoDO = 19;
 int thermoCS = 23;
 int thermoCLK = 18;
@@ -13,18 +18,25 @@ void setup() {
   Serial.begin(9600);
 
   Serial.println("MAX6675 test");
+  dht.begin();
+
   // wait for MAX chip to stabilize
   delay(500);
 }
 
 void loop() {
-  // basic readout test, just print the current temp
-  
-   Serial.print("C = "); 
-   Serial.println(thermocouple1.readCelsius());
-   Serial.print("C = ");
-   Serial.println(thermocouple2.readCelsius());
- 
-   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
-   delay(1000);
+  Serial.println("Hum = ");
+  Serial.print(dht.readHumidity());
+  Serial.print("   ");
+  Serial.println("Temp = ");
+  Serial.print(dht.readTemperature());
+
+  Serial.println("C1 = ");
+  Serial.print(thermocouple1.readCelsius());
+  Serial.print("   ");
+  Serial.print("C2 = ");
+  Serial.print(thermocouple2.readCelsius());
+
+  // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
+  delay(1000);
 }
